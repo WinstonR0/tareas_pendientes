9eaa7ddb54a8195de8d08a0272ef06269088bc16
Rosdri_Linux <wuistonesneiderrodri@gmail.com>
2026-05-03 09:11:59 -0500

    nose

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // URL base de la api
-const API_URL = "http://localhost:3000/api/tareas";
+const API_URL = "https://tu-app.onrender.com/api/tareas";
 
 // 1. cargar las taeas al iniciar 
 document.addEventListener("DOMContentLoaded", obtenerTareas);
